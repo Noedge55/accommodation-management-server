a5b0daeeb98b4ffe66fd5cc27b7e72e16f3a2a0a
noedge <noedge@yeah.net>
2019-09-01 18:15:15 +0800

    订单页面开发

--- src/main/java/org/noedge/dao/CheckInOrderDao.java
+++ src/main/java/org/noedge/dao/CheckInOrderDao.java
@@ -1,10 +1,14 @@
 package org.noedge.dao;
 
+import org.noedge.domain.Result;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
 
 /**
  * @Description:
  */
 @Repository("checkInOrderDao")
 public interface CheckInOrderDao {
+    public Result getOrders(Map param);
 }
